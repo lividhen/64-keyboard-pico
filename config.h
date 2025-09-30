863479f9717f354d28810cb9f0d335c310bd8bf4
lividhen <jaredrskinner@gmail.com>
2025-09-29 19:17:19 -0600

    rework

--- config.h
+++ config.h
@@ -1,16 +1,7 @@
 #pragma once
-//#include "config_common.h"
-
-//#define VENDOR_ID 0xFEED
-//#define PRODUCT_ID 0x6424
-//#define DEVICE_VER 0x0001
-//#define MANUFACTURER lividhen
-//#define PRODUCT Printed Pico
 
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 14
 
-#define MATRIX_ROW_PINS { GP7, GP22, GP11, GP18, GP15 }
-#define MATRIX_COL_PINS { GP6, GP8, GP9, GP10, GP12, GP13, GP14, GP16, GP17, GP19, GP20, GP21, GP23, GP24 }
-
-//#define DIODE_DIRECTION COL2ROW
+//#define MATRIX_ROW_PINS { GP7, GP22, GP11, GP18, GP15 }
+//#define MATRIX_COL_PINS { GP6, GP8, GP9, GP10, GP12, GP13, GP14, GP16, GP17, GP19, GP20, GP21, GP23, GP24 }
